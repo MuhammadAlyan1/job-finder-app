3f1716207aa0bf7a0f6f1724fa030464f1311fd7
Muhammad Alyan <alyan0332@gmail.com>
2022-10-12 20:45:58 +0500

    add filter component

---
+++ components/filter/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DesktopSearchbar from './DesktopSearchbar';
+import MobileSearchbar from './MobileSearchbar';
+
+const Filter = () => {
+  return (
+    <>
+      <DesktopSearchbar />
+      <MobileSearchbar />
+    </>
+  );
+};
+
+export default Filter;
